54fbeb03c874b12f4a529c77baaee73ac546d9b5
Spartan <spartan@IMS-007MBA.local>
2023-05-04 00:29:04 -0700

    usertab

---
+++ book-ui/src/components/user/UserTab.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Tab } from 'semantic-ui-react'
+import ClassesTable from "./ClassesTable";
+
+
+function UserTab(props) {
+
+    const { handleInputChange, updateMeetState, month, isForMember, isClassesLoading } = props
+    const { isBooksLoading, books, bookIsbn, bookTitle, bookTextSearch, handleAddBook, handleDeleteBook, handleSearchBook } = props
+    const { classes, classesTitle, classesDescription, handleAddClasses, isClassForMember, handleDeleteClasses, handleAddRegisteredClasses, handleGetRegisteredClasses } = props
+    const { instructorIdForClassCreate, isInstructorsLoading, instructorAge, handleDeleteInstructor, instructorDescription, instructors, instructorEmail, instructorName, handleAddInstructor, printRegisteredClasses } = props
+
+    const panes = [
+        {
+            menuItem: { key: 'classes', icon: 'book', content: 'Classes' },
+            render: () => (
+                <Tab.Pane loading={isClassesLoading}>
+                    <ClassesTable
+                        classes={classes}
+                        classesTitle={classesTitle}
+                        classesDescription={classesDescription}
+                        bookTextSearch={bookTextSearch}
+                        handleInputChange={handleInputChange}
+                        handleAddClasses={handleAddClasses}
+                        handleDeleteBook={handleDeleteBook}
+                        handleSearchBook={handleSearchBook}
+                        isClassForMember={isClassForMember}
+                        handleAddRegisteredClasses={handleAddRegisteredClasses}
+                        handleGetRegisteredClasses={handleGetRegisteredClasses}
+                        instructors={instructors}
+                        instructorIdForClassCreate={instructorIdForClassCreate}
+                        printRegisteredClasses={printRegisteredClasses}
+                    />
+                </Tab.Pane>
+            )
+        }
+
+    ]
+
+    return (
+        <Tab menu={{ attached: 'top' }} panes={panes} />
+    )
+}
+
+export default UserTab
